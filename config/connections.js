f5518a725900c79bc935dab95e379c5356a55334
abstractl0gic <zwlyons@gmail.com>
2023-09-24 23:00:50 -0500

    add user model

--- config/connections.js
+++ config/connections.js
@@ -1,5 +1,16 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/developersApplications');
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/social_network', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB database');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
 
-module.exports = connection;
+module.exports = connectDB;
